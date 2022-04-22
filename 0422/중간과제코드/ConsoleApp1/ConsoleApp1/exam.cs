7c7acb9b18b8d693f00542c8ff002ba549cdf1dc
DoGyun Kim <ehrbs4789@gnami.com>
2022-04-22 20:07:00 +0900

    0422

---
+++ 0422/중간과제코드/ConsoleApp1/ConsoleApp1/exam.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class exam
+    {
+        internal static void InternalV()
+        {
+            Console.WriteLine("Internal");
+        }
+        public static void PublicV()
+        {
+            Console.WriteLine("public");
+        }
+        private static void PrivateV()
+        {
+            Console.WriteLine("Private");
+        }
+        protected static void ProtectedV()
+        {
+            Console.WriteLine("Protected");
+        }
+        /*public static void Main()
+        {
+            exam.InternalV();
+            exam.PublicV();
+            exam.PrivateV();
+            exam.ProtectedV();
+        }*/
+
+    }
+    public class innerclass
+    {
+       /* public static void Main()
+        {
+            exam.InternalV();
+            exam.PublicV();
+        }*/
+    }
+
+    public class Outerclass : exam
+    {
+        /*public static void Main()
+        {
+            exam.InternalV();
+            exam.PublicV();
+            exam.ProtectedV();
+        }*/
+    }
+}
---
+++ 0422/중간과제코드/ConsoleApp1/ConsoleApp1/exam1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace TestApp0
+{
+    internal static class InternalClass
+    {
+        internal static Int32 InternalMethod()
+        {
+            return 777;
+        }
+    }
+
+    public class TestClass
+    {
+        public static Int32 TestMethod()
+        {
+            /* 같은 어셈블리이므로 접근이 가능함 */
+            return InternalClass.InternalMethod();
+        }
+    }
+}
+
+/* Assembly: TestApp1 */
+namespace TestApp1
+{
+    public class TestClass
+    {
+        public static void TestMetho()
+        {
+            // InternalClass 에 접근 불가,
+            // 서로 다른 어셈블리임
+            TestApp0.InternalClass.InternalMethod();
+        }
+    }
+}
