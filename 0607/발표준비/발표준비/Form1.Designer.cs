433e9bf3eed435ee29cb3168fc6312d27d081a6d
DoGyun Kim <ehrbs4789@gnami.com>
2022-06-07 20:45:00 +0900

    0607

---
+++ 0607/발표준비/발표준비/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace 발표준비
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ID_txt = new System.Windows.Forms.TextBox();
+            this.PW_txt = new System.Windows.Forms.TextBox();
+            this.login = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ID_txt
+            // 
+            this.ID_txt.Location = new System.Drawing.Point(277, 121);
+            this.ID_txt.Name = "ID_txt";
+            this.ID_txt.Size = new System.Drawing.Size(100, 25);
+            this.ID_txt.TabIndex = 0;
+            // 
+            // PW_txt
+            // 
+            this.PW_txt.Location = new System.Drawing.Point(277, 171);
+            this.PW_txt.Name = "PW_txt";
+            this.PW_txt.Size = new System.Drawing.Size(100, 25);
+            this.PW_txt.TabIndex = 1;
+            // 
+            // login
+            // 
+            this.login.Location = new System.Drawing.Point(434, 121);
+            this.login.Name = "login";
+            this.login.Size = new System.Drawing.Size(125, 75);
+            this.login.TabIndex = 2;
+            this.login.Text = "button1";
+            this.login.UseVisualStyleBackColor = true;
+            this.login.Click += new System.EventHandler(this.login_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.login);
+            this.Controls.Add(this.PW_txt);
+            this.Controls.Add(this.ID_txt);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox ID_txt;
+        private System.Windows.Forms.TextBox PW_txt;
+        private System.Windows.Forms.Button login;
+    }
+}
+
---
+++ 0607/발표준비/발표준비/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 발표준비
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void login_Click(object sender, EventArgs e)
+        {
+            if(ID_txt.ToString() == "kimdg1122")
+            {
+                if(PW_txt.ToString() == "ehrbs@2997")
+                {
+                    this.Hide();
+
+                    Form2 f2 = new Form2();
+                    f2.Show();
+                }
+            }
+            else
+            {
+                MessageBox.Show("로그인이 완료 되었습니다. 다음화면으로 넘어갑니다.");
+            }
+        }
+    }
+}
